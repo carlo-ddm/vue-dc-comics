3f4449c8b1064b2f70377ea018fe9e2055c603e7
carlo-ddm <carloduminuco90@gmail.com>
2022-05-13 17:42:27 +0200

    creazione macro-strutture

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    Header
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    Main
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'MainComponent'
+}
+</script>
+
+<style>
+
+</style>
