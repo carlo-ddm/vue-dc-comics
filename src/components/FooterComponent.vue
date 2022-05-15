0681c35b5b700e66fee9eb70b10c6e4359f7e181
carlo-ddm <carloduminuco90@gmail.com>
2022-05-15 13:26:45 +0200

    costruzione layout footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,17 +1,130 @@
 <template>
   <footer>
     <div class="container">
-      Footer
+      <div class="up">
+        <div class="left">
+          <div class="col">
+            <h2>dc comics</h2>
+            <!-- Prima ul -->
+            <ul>
+              <li v-for="(link,index) in links" :key="`link_${index}`" >
+                <a href="">{{link.name}}</a>
+              </li>
+            </ul>
+            <!--  Seconda ul -->
+            <h2>Title 2</h2>
+            <ul>
+              <li>
+                <a href=""></a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="col">
+            <h2>Title</h2>
+            <ul>
+              <li>
+                <a href=""></a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="col">
+            <h2>Title</h2>
+            <ul>
+              <li>
+                <a href=""></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="right">DX</div>
+      </div>
+      <div class="bottom"></div>
     </div>
   </footer>
 </template>
 
 <script>
 export default {
-  name: 'FooterComponent'
-}
+  name: "FooterComponent",
+  data(){
+    return {
+      links: [
+        {
+        name: 'caracters',
+        href: '#'
+      },
+      {
+        name: 'comics',
+        href: '#'
+      },
+      {
+        name: 'movies',
+        href: '#'
+      },
+      {
+        name: 'tv',
+        href: '#'
+      },
+      {
+        name: 'games',
+        href: '#'
+      },
+      {
+        name: 'news',
+        href: '#'
+      },
+      {
+        name: 'caracters',
+        href: '#'
+      },
+      ]
+    }
+  }
+};
 </script>
 
 <style lang="scss" scope>
+//import
+@import "../assets/styles/mixines";
 
+footer {
+  background-image: url("../assets/img/footer-bg.jpg");
+}
+
+h2 {
+  margin: 20px 0;
+  text-transform: uppercase;
+}
+
+.up {
+  @include d-flex-jc-ac;
+  color: white; //rimuovi
+  padding: 10px;
+  border: 1px solid black;
+  height: 400px;
+
+  .left {
+    @include d-flex-jc-ac;
+    @include l-r-footer;
+    .col {
+      width: calc(100% / 3);
+      height: 100%;
+      border: 2px solid white;
+      ul {
+        li {
+          list-style: none;
+          a{
+            @include footer-links;
+          }
+        }
+      }
+    }
+  }
+  .right {
+    @include l-r-footer;
+  }
+}
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,11 +1,11 @@
 <template>
   <header>
     <div class="container">
-      <img src="../assets/img/dc-logo.png" alt="dc_logo">
+      <img src="../assets/img/dc-logo.png" alt="dc_logo" />
       <ul>
-        <li v-for="(item,index) in menu" :key="`menu_${index}`">
+        <li v-for="(item, index) in menu" :key="`menu_${index}`">
           <a href="item.href">
-            {{item.name}}
+            {{ item.name }}
           </a>
         </li>
       </ul>
@@ -15,73 +15,72 @@
 
 <script>
 export default {
-  name: 'HeaderComponent',
-  data(){
-    return{
-      menu:[
+  name: "HeaderComponent",
+  data() {
+    return {
+      menu: [
         {
-          href: '#',
-          name: 'characters',
-          active: false
+          href: "#",
+          name: "characters",
+          active: false,
         },
         {
-          href: '#',
-          name: 'comics',
-          active: true
+          href: "#",
+          name: "comics",
+          active: true,
         },
         {
-          href: '#',
-          name: 'movies',
-          active: false
+          href: "#",
+          name: "movies",
+          active: false,
         },
         {
-          href: '#',
-          name: 'tv',
-          active: false
+          href: "#",
+          name: "tv",
+          active: false,
         },
         {
-          href: '#',
-          name: 'games',
-          active: false
+          href: "#",
+          name: "games",
+          active: false,
         },
         {
-          href: '#',
-          name: 'collectibles',
-          active: false
+          href: "#",
+          name: "collectibles",
+          active: false,
         },
         {
-          href: '#',
-          name: 'videos',
-          active: false
+          href: "#",
+          name: "videos",
+          active: false,
         },
         {
-          href: '#',
-          name: 'fans',
-          active: false
+          href: "#",
+          name: "fans",
+          active: false,
         },
         {
-          href: '#',
-          name: 'news',
-          active: false
+          href: "#",
+          name: "news",
+          active: false,
         },
         {
-          href: '#',
-          name: 'shop',
-          active: false
+          href: "#",
+          name: "shop",
+          active: false,
         },
-      ]
-      
-    }
-  }
-}
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scope>
 //import
-@import '../assets/styles/mixines';
-@import '../assets/styles/utilities';
+@import "../assets/styles/mixines";
+@import "../assets/styles/utilities";
 
-header .container{
+header .container {
   @include d-flex-jc-ac;
   height: 100px;
 
@@ -92,23 +91,15 @@ header .container{
 
   ul {
     @include d-flex-jc-ac;
-    
+
     li {
       @include d-flex-jc-ac;
       list-style: none;
 
       a {
-        margin: 0 10px;
-        text-decoration: none;
-        text-transform: uppercase;
-        font-size: 12px;
-        font-weight: bold;
-        color: #363636;
+        @include link;
       }
     }
-
-    }
-    
+  }
 }
-
 </style>
